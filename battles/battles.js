473028b5a7d49df079ce4d2eaec9e50dae480bf1
daniel <developmentleviathan@gmail.com>
2019-06-23 11:32:42 -0400

    battles

--- battles/battles.js
+++ battles/battles.js
@@ -2,7 +2,8 @@ var base = 'https://smallfolio.bitnamiapp.com/battles/';
 
 Vue.component('stat-header', {
   props: ['stat'],
-  template: '<li class="stat"><a @click="stats.changeChannel(stat.channel)"> \
+  template: '<li class="stat"> \
+    <a @click="artists.loadNewChannel(stat.channel)" style="color: black;"> \
     {{ stat.channel }}</a></li>'
 })
 
@@ -10,11 +11,7 @@ var title = new Vue({
     el: '#title',
     methods: {
         goHome: function () {
-            artists.visible = true;
-            artistBattles.visible = false;
-            battle.visible = false
-            $("#yt").html('');
-            artistBattles.battles = [];
+            clearForHome();
         }
     }
 })
@@ -30,24 +27,11 @@ var stats = new Vue({
             $.getJSON(base + 'random', function (json) {
                 battle.getBattle(json.battle[0].battle_id);
             });
-        },
-        changeChannel: function (channel) {
-            artists.visible = true;
-            artistBattles.visible = false;
-            artistBattles.battles = [];
-            battle.visible = false
-            $("#yt").html('');
-
-            artists.loadNewChannel(channel);
         }
     },
     created: function () {
         $.getJSON(base + 'stats', function (json) {
-            json.stats.forEach(function(item) {
-                stats.stats.push(item);
-
-            });
-
+            stats.stats = json.stats;
             stats.visible = true;
         });
     }
@@ -63,17 +47,15 @@ var artists = new Vue({
         visible: true
     },
     methods: {
-        loadArtist: function (artist, channel) {
-            artists.visible = false;
-            artistBattles.getBattles(artist, channel);
-        },
         loadNewChannel: function (channel) {
+            var end = base + 'artists/' + channel + '/' + artists.counter;
+
+            clearForHome();
+
             artists.artists = [];
             artists.counter = 0;
 
-            $.getJSON(base + 'artists/' + channel + '/' + artists.counter,
-                function (json){
-
+            $.getJSON(end, function (json) {
                 json.artists.forEach(function(item) {
                     artists.artists.push(item);
                 });
@@ -83,24 +65,22 @@ var artists = new Vue({
             });
         },
         loadMore: function (event) {
-            artists.counter++;
+            var end = base + 'artists/' + artists.toLoad + '/' +
+                ++artists.counter;
+
             artists.loadingText = 'Loading...'
 
-            var end = 'artists/' + artists.toLoad + '/' + artists.counter;
-
-            $.getJSON(base + end,
-                function (json){
-
+            $.getJSON(end, function (json) {
                 json.artists.forEach(function(item) {
                     artists.artists.push(item);
                 });
 
-                artists.loadingText = 'load more'
+                artists.loadingText = 'Load more'
             });
         }
     },
     created: function () {
-        $.getJSON(base + 'artists/theurltv/0', function (json){
+        $.getJSON(base + 'artists/total/0', function (json){
             json.artists.forEach(function(item) {
               artists.artists.push(item);
             });
@@ -114,31 +94,24 @@ var artistBattles = new Vue({
         artist: '',
         channel: '',
         battles: [],
-        visible: false,
-        battle_count: '',
-        views: '',
-        average: '',
-        percentage: ''
+        info: [],
+        visible: false
     },
     methods: {
         loadBattle: function(battleID) {
             battle.getBattle(battleID);
         },
         getBattles: function (artist, channel) {
+            var end = base + 'artist/' + channel + '/' + artist;
+
             artistBattles.artist = artist;
             artistBattles.channel = channel;
 
-            $.getJSON(base + 'artist/' + channel + '/' + artist,
-                function (json) {
-
-                var info = json.artist_info;
-
-                artistBattles.battle_count = info.battle_count;
-                artistBattles.views = info.battle_views;
-                artistBattles.average = info.views_average;
-                artistBattles.percentage = info.views_percentage;
-
+            $.getJSON(end, function (json) {
+                artistBattles.info = json.artist_info;
                 artistBattles.battles = json.artist_battles;
+
+                artists.visible = false;
                 artistBattles.visible = true;
             });
         }
@@ -151,28 +124,39 @@ var artistBattles = new Vue({
 var battle = new Vue({
     el: '#battle',
     data: {
-        battleID: 0,
         battle: [],
         visible: false
     },
     methods: {
         getBattle: function (battleID) {
-            battle.visible = true;
-            artists.visible = false;
-            artistBattles.visible = false;
+            $.getJSON(base + 'battle/' + battleID, function (json) {
+                battle.battle = json.battle[0];
 
-            value = "<div class=\"videoWrapper\"><iframe width='100%' \
-                height='100%' \
-                src='http://www.youtube.com/embed/" + battleID + "' \
-                frameborder='0' allowFullScreen></iframe></div>";
+                value = "<div class=\"videoWrapper\"><iframe width='100%' \
+                    height='100%' \
+                    src='https://www.youtube.com/embed/" + battleID + "' \
+                    frameborder='0' allowFullScreen></iframe></div>";
 
-            $("#yt").html(value);
+                $("#yt").html(value);
+
+                artists.visible = false;
+                artistBattles.visible = false;
+                battle.visible = true;
+            });
         }
     },
     created: function () {
 
     }
 })
+
+function clearForHome() {
+   artists.visible = true;
+    artistBattles.visible = false;
+    battle.visible = false
+    $("#yt").html('');
+    artistBattles.battles = [];
+}
 
 function commas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
